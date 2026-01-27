388f9a68b677af4901a4971b069d47d05815a151
NoraBld <norabelaid86@gmail.com>
2026-01-27 18:52:58 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Competence from "./pages/Competence";
+import Competence from "./pages/competence";
 import Contact from "./pages/Contact";
 import Diplomeetstage from "./pages/Diplomeetstage";
 import Mesprojet from "./pages/Mesprojet";
